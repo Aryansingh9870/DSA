d17409c971dabca7032bde0c2baa71ebf051f4be
Aryan Singh <wintersoldier9870@gmail.com>
2026-04-13 04:49:34 +0530

    initial commit

---
+++ Arryas and Hashsets/hashsets.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class hashsets {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5,1};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        
+        for(int i = 0;i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}

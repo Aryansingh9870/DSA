7e9c51f6e9ff73c5cfe2b509592534f613f5ff4d
Aryan Singh <wintersoldier9870@gmail.com>
2026-04-13 09:56:38 +0530

    HashSet

--- Arryas and Hashsets/hashsets.java
+++ Arryas and Hashsets/hashsets.java
@@ -1,15 +1,33 @@
 import java.util.HashSet;
 
+// class hashsets{
+//     public boolean Duplicate(int[] nums){
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i=0;i<nums.length;i++){
+//             if(set.contains(nums[i])){
+//                 return true;
+//             }
+//             set.add(nums[i]);
+//         }
+//         return false;
+//     }
+// }
+
 public class hashsets {
     public static void main(String[] args) {
         int[] arr = {1,3,4,5,1};
-
         HashSet<Integer> set = new HashSet<>();
 
-        
         for(int i = 0;i < arr.length; i++){
-            System.out.println(arr[i]);
+
+            if(set.contains(arr[i])){
+                System.out.println("True");
+                return;
+            }
+            set.add(arr[i]);
         }
+        System.out.println("False");
     }
     
 }

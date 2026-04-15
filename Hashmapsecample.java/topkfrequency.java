8463f9989e8931f1b049e8e6ac1c8e682607e4d1
Aryansingh9870 <wintersoldier9870.com>
2026-04-16 01:32:26 +0530

    topkfrequency

---
+++ Hashmapsecample.java/topkfrequency.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class topkfrequency {
+    public static void main(String[] args) {
+        int[] a = {1,1,1,2,2,3};
+        int k = 2;
+        int[] result = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : a){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (a1, a2) -> a2.getValue() - a1.getValue());
+        for(int i = 0;i<k;i++){
+            result[i] =list.get(i).getKey();
+        }
+        System.out.println(Arrays.toString(result));
+    }
+    
+}

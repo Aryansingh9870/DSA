520ae23b0c72d63f8cfc48689aa0af7ee53849e0
Aryansingh9870 <wintersoldier9870.com>
2026-04-21 13:24:53 +0530

    longconsec

--- Hashmapsecample.java/topkfrequency.java
+++ Hashmapsecample.java/topkfrequency.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class topkfrequency {
     public static void main(String[] args) {
         int[] a = {1,1,1,2,2,3};
@@ -23,4 +24,18 @@ public class topkfrequency {
         System.out.println(Arrays.toString(result));
     }
     
-}
+} 
+
+// public int[] topKFrequent(int[] nums, int k) {
+//     HashMap<Integer, Integer> map = new HashMap<>();
+//     for(int num : nums){
+//         map.put(num, map.getOrDefault(num, 0) + 1);
+//     }
+//     List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+//     Collections.sort(list, (a1, a2) -> a2.getValue() - a1.getValue());
+//     int[] result = new int[k];
+//     for(int i = 0;i<k;i++){
+//         result[i] =list.get(i).getKey();
+//     }
+//     return result;
+// }
